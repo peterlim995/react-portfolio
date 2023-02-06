90bf6c5155cd16fd9592f855f8456d108a494c5b
unknown <peterlim995@gmail.com>
2023-02-05 22:56:50 -0500

    more project

--- src/components/Project.js
+++ src/components/Project.js
@@ -17,8 +17,8 @@ export default function Project({ images }) {
                             <a href={github} target="_blank" className="github"><FaGithub style={{ fontSize: '50px' }}/></a>
                         </div>
                         <div className="card-body">
-                            <h4 className="card-title">{title}</h4>
-                            <p className="card-text">{description}</p>
+                            <h4 className="card-title pt-2">{title}</h4>
+                            <p className="card-text pb-3">{description}</p>
                         </div>
                     </div>
                 ))}
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,34 +1,40 @@
 import React, {useState} from 'react';
 import Project from '../Project';
-import quizjpg from '../../images/quiz.jpg';
-
 
 export default function Portfolio() {
 
   const images = [
     {
-      src: '/react-portfolio/images/quiz.jpg',
-      alt: 'Code Quiz',
-      title: 'Code Quiz',
-      description: 'Web-APIs Challenge',
-      deployed: 'https://peterlim995.github.io/Code_Quiz/index.html',
-      github: 'https://github.com/peterlim995/Code_Quiz'
+      src: '/react-portfolio/images/Social-Network-API.jpg',
+      alt: 'Social Network API',
+      title: 'Social Network API',
+      description: 'NoSQL Challenge',
+      deployed: 'https://user-images.githubusercontent.com/115592345/215306973-72b365c4-2d7f-4f89-9caf-a9f8963806ea.mp4',
+      github: 'https://github.com/peterlim995/Social_Network_API'
     },
     {
-      src: '/react-portfolio/images/Weather_Dashboard.jpg',
-      alt: 'Weather Dashboard',
-      title: 'Weather Dashboard',
-      description: 'Server-Side APIs Challenge',
-      deployed: 'https://peterlim995.github.io/Weather_Dashboard/',
-      github: 'https://github.com/peterlim995/Weather_Dashboard'
+      src: '/react-portfolio/images/community-chat.jpg',
+      alt: 'Group Project 2',
+      title: 'Group Project 2: Community Chat',
+      description: 'Interactive Full-Stack Application',
+      deployed: 'https://community-chat.herokuapp.com/',
+      github: 'https://github.com/BillStephens2022/Community-Chat'
     },
     {
-      src: '/react-portfolio/images/note-taker.jpg',
-      alt: 'Note Taker',
-      title: 'Note Taker',
-      description: 'Express.js Challenge',
-      deployed: 'https://note-taker-peterlim995.herokuapp.com/',
-      github: 'https://github.com/peterlim995/Note_Taker'
+      src: '/react-portfolio/images/tech-blog.jpg',
+      alt: 'Tech Blog',
+      title: 'Tech Blog',
+      description: 'Model-View-Controller (MVC) Challenge',
+      deployed: 'https://tech-blog-peterlim995.herokuapp.com/',
+      github: 'https://github.com/peterlim995/Tech_Blog'
+    },
+    {
+      src: '/react-portfolio/images/E-Commerce-Backend.jpg',
+      alt: 'E-Commerce Back End',
+      title: 'E-Commerce Back End',
+      description: 'Object-Relational Mapping (ORM) Challenge',
+      deployed: 'https://user-images.githubusercontent.com/115592345/211721616-05e66df1-94bf-4ab4-9fb0-fc385aeae52a.mp4',
+      github: 'https://github.com/peterlim995/E-Commerce-Back-End'
     },
     {
       src: '/react-portfolio/images/employee-tracker.jpg',
@@ -39,6 +45,14 @@ export default function Portfolio() {
       github: 'https://github.com/peterlim995/Employee_Tracker'
     },
     {
+      src: '/react-portfolio/images/note-taker.jpg',
+      alt: 'Note Taker',
+      title: 'Note Taker',
+      description: 'Express.js Challenge',
+      deployed: 'https://note-taker-peterlim995.herokuapp.com/',
+      github: 'https://github.com/peterlim995/Note_Taker'
+    },
+    {
       src: '/react-portfolio/images/Team-profile-generator.jpg',
       alt: 'Team Profile Generator',
       title: 'Team Profile Generator',
@@ -47,13 +61,58 @@ export default function Portfolio() {
       github: 'https://github.com/peterlim995/Team_Profile_Generator'
     },
     {
+      src: '/react-portfolio/images/Readme_Generator.jpg',
+      alt: 'Professional README Generator',
+      title: 'Professional README Generator',
+      description: 'Node.js Challenge',
+      deployed: 'https://user-images.githubusercontent.com/115592345/207998333-3681d070-f8bf-4fef-bee0-3042701747ed.mp4',
+      github: 'https://github.com/peterlim995/README-Generator'
+    },
+    {
       src: '/react-portfolio/images/healthy-gigs.jpg',
       alt: 'Group Project 1',
       title: 'Group Project 1: Healthy Gigs',
       description: 'Interactive Front-End Application',
       deployed: 'https://sam-lerner.github.io/healthy-gigs/',
       github: 'https://github.com/sam-lerner/healthy-gigs'
-    }
+    },
+    {
+      src: '/react-portfolio/images/Weather_Dashboard.jpg',
+      alt: 'Weather Dashboard',
+      title: 'Weather Dashboard',
+      description: 'Server-Side APIs Challenge',
+      deployed: 'https://peterlim995.github.io/Weather_Dashboard/',
+      github: 'https://github.com/peterlim995/Weather_Dashboard'
+    },
+    {
+      src: '/react-portfolio/images/Work-Day-Scheduler_.jpg',
+      alt: 'Work Day Scheduler',
+      title: 'Work Day Scheduler',
+      description: 'Third-Party APIs',
+      deployed: 'https://peterlim995.github.io/Work-Day-Scheduler/',
+      github: 'https://github.com/peterlim995/Work-Day-Scheduler'
+    },
+    {
+      src: '/react-portfolio/images/quiz.jpg',
+      alt: 'Code Quiz',
+      title: 'Code Quiz',
+      description: 'Web-APIs Challenge',
+      deployed: 'https://peterlim995.github.io/Code_Quiz/index.html',
+      github: 'https://github.com/peterlim995/Code_Quiz'
+    },
+    {
+      src: '/react-portfolio/images/GeneratePassword_.png',
+      alt: 'Password Generator',
+      title: 'Password Generator',
+      description: 'JavaScript Challenge',
+      deployed: 'https://peterlim995.github.io/GeneratePassword/',
+      github: 'https://github.com/peterlim995/GeneratePassword'
+    },
+    
+    
+    
+    
+    
 
   ]
 
